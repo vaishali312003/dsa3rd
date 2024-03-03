84e5b9954e7f6bd68b361a90e176c32b2cf1bde5
vaishali312003 <va3har@gmail.com>
2024-03-03 18:23:27 +0530

    handling error

--- linkedlist.java
+++ linkedlist.java
@@ -3,8 +3,8 @@ public class linkedlist {
     public static void main(String[] args) {
         Node node1 = new Node(1);
         Node node2=new Node(2);
-        node1->next=node2;
-        
+        node1.next=node2;
+
         printLinkedlist(node1);
     }
 
